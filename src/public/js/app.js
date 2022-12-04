f09373442e748574f8ea0f301f9194333be81e7b
jthebat <taein007@gmail.com>
2022-12-04 14:12:16 +0900

    CHAT WITH WEBSOCKETS

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,37 @@
-alert("hi");
+const messsageList = document.querySelector("ul");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload) {
+    const msg = { type, payload };
+    return JSON.stringify(msg);
+}
+
+socket.addEventListener("open",()=>{
+    console.log("Connected to Server ✔");
+});
+
+socket.addEventListener("close",()=>{
+    console.log("Disconnected from Server ❌");
+});
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(makeMessage("new_message", input.value));
+    const li = document.createElement("li");
+    li.innerText = `You: ${input.value}`;
+    messsageList.append(li);
+    input.value="";
+}
+
+function handleNickSubmit(event) {
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+    socket.send(makeMessage("nickname",input.value));
+    input.value="";    
+}
+
+messageForm.addEventListener("submit",handleSubmit);
+nickForm.addEventListener("submit",handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -1,13 +1,43 @@
+import http from "http";
 import express from "express";
-
+import WebSocket from "ws";
+import { SocketAddress } from "net";
+ 
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
-
+app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req,res) => res.render("home"));
 app.get("/*",(req,res) => res.redirect("/"));
-app.use("/public", express.static(__dirname + "/public"));
+
 
 const handleListen = () => console.log('Listening on http://localhost:3000');
-app.listen(3000,handleListen);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({server});
+
+//fake DB
+const sockets = [];
+
+wss.on("connection", (socket)=>{
+    sockets.push(socket);
+    socket["nickname"] = "Anony"
+    console.log("Connected to Browser ✔");
+    socket.on("close",()=>{
+        console.log("Disconnected from the Browser ❌");
+    });
+    socket.on("message",(msg)=>{
+        const message = JSON.parse(msg.toString("utf-8"));
+        //console.log(parsed);
+        switch(message.type) {
+            case "new_message":
+                sockets.forEach(aSocket => aSocket.send(`${socket.nickname}: ${message.payload}`)); 
+            case "nickname":
+                socket["nickname"] = message.payload;
+        }       
+    });
+    socket.send("hello");
+});
+
+server.listen(3000,handleListen);
